9f9fe36bbe3e1d930de18050e28be1ab5c2e50e9
Nama Kamu <email@example.com>
2025-05-10 11:40:24 +0700

    delete navlink.tsx

--- src/app/components/navlink.tsx
+++
@@ -1,9 +0,0 @@
-const NavLink = (href: string, tittle: string) => {
-    return (
-        <a href= {href} className="block py-2 pl-2 pr-4 text-white">
-            {tittle}
-        </a>
-    )
-}
-
-export default NavLink
